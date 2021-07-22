c19f5fabfa12bed09148805dbebcd8d097409642
jeffersonkg745 <77896216+jeffersonkg745@users.noreply.github.com>
2021-07-22 13:00:33 -0400

    Added correct title for ReservationPage. Added beginning of recycler view that will go in each of the fragments to display future/pending/past reservations in a vertical list. Added new VenueObjectReservationPg b/c of layout difference with the current VenueObject.

--- app/src/main/java/edu/neu/venuify/reservationPage/ReservationPageActivity.java
+++ app/src/main/java/edu/neu/venuify/reservationPage/ReservationPageActivity.java
@@ -8,15 +8,28 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 //import edu.neu.venuify.reservationPage.databinding.ActivityReservationPage1Binding;
 
-import edu.neu.venuify.R;
+import java.util.ArrayList;
 
+import edu.neu.venuify.R;
+import edu.neu.venuify.reservationPage.RecyclerViewForFragments.RecyclerViewAdapter;
+import edu.neu.venuify.reservationPage.RecyclerViewForFragments.VenueObjectReservationPg;
+import edu.neu.venuify.reservationPage.TabLayoutAndFragments.PastListOfInvitesFragment;
+import edu.neu.venuify.reservationPage.TabLayoutAndFragments.PendingListOfInvitesFragment;
+import edu.neu.venuify.reservationPage.TabLayoutAndFragments.TabPagerAdapter;
+import edu.neu.venuify.reservationPage.TabLayoutAndFragments.UpcomingListOfInvitesFragment;
+
+/**
+ * Adapted from Chapter 47, Android Studio 4.1 Development Essentials
+ */
 public class ReservationPageActivity extends AppCompatActivity implements UpcomingListOfInvitesFragment.OnFragmentInteractionListener,
-        PendingListOfInvitesFragment.OnFragmentInteractionListener, PastListOfInvitesFragment.OnFragmentInteractionListener{
+        PendingListOfInvitesFragment.OnFragmentInteractionListener, PastListOfInvitesFragment.OnFragmentInteractionListener {
 
 
     @Override
@@ -29,21 +42,28 @@ public class ReservationPageActivity extends AppCompatActivity implements Upcomi
     }
 
     private void configureTabLayout() {
+
+        //sets the tabs with corresponding names
         TabLayout tabLayout = findViewById(R.id.Tab_layout);
         tabLayout.addTab(tabLayout.newTab().setText("Upcoming Invites"));
         tabLayout.addTab(tabLayout.newTab().setText("Pending Invites"));
         tabLayout.addTab(tabLayout.newTab().setText("Past Reservations"));
 
+        //get reference for pager instance, and make an instance of the TabPagerAdapter
+        //adapter is set as the adapter for the viewPager
         final ViewPager viewPager = findViewById(R.id.pager);
         final PagerAdapter adapter = new TabPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(adapter);
 
+        //tabLayout is added to the page change listener
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
+                //recyclerview doesn't work yet
+                //createRecyclerView();
             }
 
             @Override
@@ -60,6 +80,26 @@ public class ReservationPageActivity extends AppCompatActivity implements Upcomi
 
     @Override
     public void onFragmentInteraction(Uri uri) {
+
+    }
+
+
+    public void createRecyclerView() {
+        //not sure about context yet
+        RecyclerView.LayoutManager recycleLayoutManager = new LinearLayoutManager(this);
+
+        //setting just the first fragment for now
+        RecyclerView recyclerView = findViewById(R.id.RecyclerViewUpcomingVenues);
+        recyclerView.setHasFixedSize(true);
+
+        //hard code a list of things for now
+        ArrayList<VenueObjectReservationPg> venueList = new ArrayList<>();
+        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(venueList);
+        recyclerView.setAdapter(recyclerViewAdapter);
+        recyclerView.setLayoutManager(recycleLayoutManager);
+
+
+
 
     }
 
--- app/src/main/java/edu/neu/venuify/reservationPage/TabLayoutAndFragments/PastListOfInvitesFragment.java
+++ app/src/main/java/edu/neu/venuify/reservationPage/TabLayoutAndFragments/PastListOfInvitesFragment.java
@@ -1,22 +1,24 @@
-package edu.neu.venuify.reservationPage;
+package edu.neu.venuify.reservationPage.TabLayoutAndFragments;
 
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import edu.neu.venuify.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link UpcomingListOfInvitesFragment#newInstance} factory method to
+ * Use the {@link PastListOfInvitesFragment#newInstance} factory method to
  * create an instance of this fragment.
+ *
+ * Adapted from Chapter 47, Android Studio 4.1 Development Essentials
+ *
  */
-public class UpcomingListOfInvitesFragment extends Fragment {
+public class PastListOfInvitesFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,7 +29,7 @@ public class UpcomingListOfInvitesFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public UpcomingListOfInvitesFragment() {
+    public PastListOfInvitesFragment() {
         // Required empty public constructor
     }
 
@@ -40,8 +42,8 @@ public class UpcomingListOfInvitesFragment extends Fragment {
      * @return A new instance of fragment UpcomingListOfInvitesFragment1.
      */
     // TODO: Rename and change types and number of parameters
-    public static UpcomingListOfInvitesFragment newInstance(String param1, String param2) {
-        UpcomingListOfInvitesFragment fragment = new UpcomingListOfInvitesFragment();
+    public static PastListOfInvitesFragment newInstance(String param1, String param2) {
+        PastListOfInvitesFragment fragment = new PastListOfInvitesFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -62,7 +64,7 @@ public class UpcomingListOfInvitesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upcoming_list_of_invites, container, false);
+        return inflater.inflate(R.layout.fragment_past_list_of_invites, container, false);
     }
     public interface OnFragmentInteractionListener {
 
