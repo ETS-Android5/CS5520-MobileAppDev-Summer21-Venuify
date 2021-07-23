66143dcbc23ad475e808d182c90ba033070f1462
leorafink1 <fink.le@husky.neu.edu>
2021-07-22 22:20:34 -0400

    newest update

--- app/src/main/java/edu/neu/venuify/reservationPage/ReservationPageActivity.java
+++ app/src/main/java/edu/neu/venuify/reservationPage/ReservationPageActivity.java
@@ -108,6 +108,8 @@ public class ReservationPageActivity extends AppCompatActivity implements Upcomi
 
 
 
+
+
     /*
     private AppBarConfiguration appBarConfiguration;
     private ActivityReservationPage1Binding binding;
