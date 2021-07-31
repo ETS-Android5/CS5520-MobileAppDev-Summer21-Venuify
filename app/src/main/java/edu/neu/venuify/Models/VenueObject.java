406e9d4e6e7b99aeef871748376122ae5ca9d05d
mmeuse <meuse.m@northeastern.edu>
2021-07-31 14:12:41 -0400

    SearchActivity fully implemented. Results are displayed in the SearchActivity. Actual searching of firebase happens in EnterSearchQuery. Refactored activity_search.xml better contrain the layout which was previously causing skipped frames. Made VenueObject implement Parceable in order to pass array list of VenueObjects from EnterSearchQuery to SearchActivity.

--- app/src/main/java/edu/neu/venuify/Models/VenueObject.java
+++ app/src/main/java/edu/neu/venuify/Models/VenueObject.java
@@ -1,6 +1,9 @@
 package edu.neu.venuify.Models;
 
-public class VenueObject {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class VenueObject implements Parcelable {
     private String venueName;
     private int imageId;
 
@@ -14,15 +17,44 @@ public class VenueObject {
         this.imageId = ImageId;
     }
 
+    public VenueObject(Parcel in) {
+        venueName = in.readString();
+        imageId = in.readInt();
+    }
+
+    public static final Creator<VenueObject> CREATOR = new Creator<VenueObject>() {
+        @Override
+        public VenueObject createFromParcel(Parcel in) {
+            return new VenueObject(in);
+        }
+
+        @Override
+        public VenueObject[] newArray(int size) {
+            return new VenueObject[size];
+        }
+    };
+
     public String getVenueName() {
         return venueName;
     }
 
     public void setVenueName(String venueName) {
+
         this.venueName = venueName;
     }
 
     public int getImageId() {
         return imageId;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(venueName);
+        dest.writeInt(imageId);
     }
 }
--- app/src/main/java/edu/neu/venuify/SearchActivity.java
+++ app/src/main/java/edu/neu/venuify/SearchActivity.java
@@ -1,18 +1,62 @@
 package edu.neu.venuify;
 
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import edu.neu.venuify.Adapters.VenueObjectAdapter;
+import edu.neu.venuify.Models.VenueObject;
 
 
 public class SearchActivity extends BaseActivity {
+    private VenueObjectAdapter venueObjectAdapter;
+    private List<VenueObject> results  = new ArrayList<>();
 
+    ActivityResultLauncher<Intent> enterSearchQueryLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult activityResult) {
+                    if (activityResult.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent intent = Objects.requireNonNull(activityResult.getData());
+                        results = intent.getParcelableArrayListExtra("data");
+                        venueObjectAdapter.setVenueObjectList(results);
+                        venueObjectAdapter.notifyDataSetChanged();
+                    }
+                    else {
+                        venueObjectAdapter.clearVenueObjectList(); //clear any previous results
+                        Toast toast = Toast.makeText(SearchActivity.this, "No Results Found", Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER,0,0);
+                        toast.show();
+                    }
+                }
 
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        RecyclerView searchRecyclerView = findViewById(R.id.searchRecyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        venueObjectAdapter = new VenueObjectAdapter(results);
+        searchRecyclerView.setAdapter(venueObjectAdapter);
+        searchRecyclerView.setLayoutManager(linearLayoutManager);
     }
 
 
@@ -24,10 +68,8 @@ public class SearchActivity extends BaseActivity {
     public int getNavigationMenuItemId() { return R.id.nav_bar_search; }
 
 
-
-    public void EnterSearchQuery(View view) {
-        startActivity(new Intent(this, EnterSearchQuery.class));
+    public void startEnterSearchQueryActivity(View view) {
+        enterSearchQueryLauncher.launch(new Intent(this, EnterSearchQuery.class));
     }
-
 
 }
