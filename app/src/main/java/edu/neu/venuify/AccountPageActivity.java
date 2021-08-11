d2e533db9a4e22e0aaf900f56cb8447be463ac62
leorafink1 <fink.le@husky.neu.edu>
2021-08-10 23:44:30 -0400

    not finished getting num reservations

--- app/src/main/java/edu/neu/venuify/AccountPageActivity.java
+++ app/src/main/java/edu/neu/venuify/AccountPageActivity.java
@@ -17,6 +17,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import edu.neu.venuify.Authentication.LoginActivity;
 import edu.neu.venuify.Entities.User;
@@ -27,6 +28,7 @@ public class AccountPageActivity extends AppCompatActivity {
     private ImageView goBack;
     FirebaseAuth mAuth;
     private DatabaseReference mDatabase;
+    final AtomicInteger count = new AtomicInteger();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -96,11 +98,42 @@ public class AccountPageActivity extends AppCompatActivity {
         goBack.setOnClickListener(view -> {
             finish();
         });
+
+
+        getNumReservations();
     }
 
     public void openLoginPage() {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    public void getNumReservations() {
+
+        mDatabase.child("reservations").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String prevChildKey) {
+                // New child added, increment count
+                Reservation reservation = dataSnapshot.getValue(Reservation.class);
+                if (reservation.user.equals(mAuth.getCurrentUser().getUid())) {
+                    int newCount = count.incrementAndGet();
+                    numReservations.setText(Integer.toString(newCount));
+                }
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String prevChildKey) {}
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String prevChildKey) {}
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+            // ...
+        });
     }
 
 }
